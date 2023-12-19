3e0098ee8feaa561c6750b4d3381895756277656
Villalba Agustin <villalba.agus.99@gmail.com>
2023-12-18 23:15:47 -0300

    reducer created

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,63 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { useReducer } from 'react'
+import { Action, type state } from './types'
+
+const initialState: state = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer (state: state, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+  return state
+}
 
 function App () {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className='App'>
       <h1>Google Traslate</h1>
